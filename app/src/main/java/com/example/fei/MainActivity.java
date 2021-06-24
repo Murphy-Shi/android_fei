52420016db629b9f0f7cbedb2f6fe742f14e1060
murphy-shi <707444832@qq.com>
2021-06-24 10:47:13 +0800

    修改参数

--- app/src/main/java/com/example/fei/MainActivity.java
+++ app/src/main/java/com/example/fei/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.fei;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.feilib.permission.PermissionHeadle;
 
 public class MainActivity extends Activity {
     private Context mContext;
@@ -29,6 +32,15 @@ public class MainActivity extends Activity {
         mListView.setOnItemClickListener((parent, view, position, id) -> {
             String str = "" + mListName[position];
             Toast.makeText(mContext, str, Toast.LENGTH_LONG).show();
+
+            PermissionHeadle permissionHeadle = new PermissionHeadle(this);
+            permissionHeadle.need(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE} );
+            permissionHeadle.requestPremission(new PermissionHeadle.Subscribe() {
+                @Override
+                public void onResult(boolean allGranted, String[] permissions) {
+                    Toast.makeText(MainActivity.this, "状态：" + allGranted, Toast.LENGTH_SHORT).show();
+                }
+            });
         });
     }
 }
